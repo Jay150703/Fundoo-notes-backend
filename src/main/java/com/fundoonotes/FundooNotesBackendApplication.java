82ce7506ba4af3ec9af50b9fd8e47711723e7872
Jay150703 <jayanthijemi15@gmail.com>
2026-04-24 11:19:23 +0530

    [Jayanthi] UC1: Project setup with package structure and dependencies

---
+++ src/main/java/com/fundoonotes/FundooNotesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fundoonotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundooNotesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundooNotesBackendApplication.class, args);
+	}
+
+}
