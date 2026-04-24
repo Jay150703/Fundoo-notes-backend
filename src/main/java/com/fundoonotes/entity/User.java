13d0d7ae07be23dcab48b0d421339cb614c2fbbb
Jay150703 <jayanthijemi15@gmail.com>
2026-04-24 11:59:50 +0530

    [Jayanthi] UC3: User entity and repository implementation

---
+++ src/main/java/com/fundoonotes/entity/User.java
@@ -0,0 +1,33 @@
+package com.fundoonotes.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    // Future ready fields (important mindset)
+    private boolean isVerified = false;
+
+}
